22a5bc7355bcc836fcae1ec9d19ac14b64dbe403
jacktingey8 <jacktingey8@gmail.com>
2026-02-24 09:54:29 -0700

    Bond, James Bond

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Console.WriteLine("Hello Prep1 World!");
+        string firstName;
+        string lastName;
+
+        Console.Write("What is your first name: ");
+        firstName=Console.ReadLine();
+
+        Console.Write("What is your last name: ");
+        lastName=Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}");
+        Console.WriteLine("Your name is " + firstName +' '+ lastName);
     }
 }
