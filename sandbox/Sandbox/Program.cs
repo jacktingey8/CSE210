1e0131b2c50fee83b055769357c25e247408e8ad
jacktingey8 <jacktingey8@gmail.com>
2026-02-25 09:59:53 -0700

    if statements test

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        Console.Write("age:");
+        int age;
+        age = int.Parse(Console.ReadLine());
+        Console.WriteLine($"You are {age}");
+
+        if (age <=100 && age != 19 && age !=0) Console.WriteLine("Ok");
+
+        else if (age == 19 || age==0) Console.WriteLine("Alright");
+        
+        else Console.WriteLine("Yes");
+        
     }
 }
