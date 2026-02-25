fbb70c7ddceeee59639f8f64f08502766eee9e75
jacktingey8 <jacktingey8@gmail.com>
2026-02-25 15:26:39 -0700

    Prep 2 grade assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("grade:");
+        int grade = int.Parse(Console.ReadLine());
+        string letterGrade;
+
+        if (grade >=90){letterGrade = "A"; }
+        else if (grade >=80 && grade <=90){letterGrade = "B"; }
+        else if (grade >=70 && grade <=80){letterGrade = "C"; }
+        else if (grade >=60 && grade <=70){letterGrade = "D"; }
+        else {letterGrade = "F"; }
+        Console.WriteLine($"Your Grade is {letterGrade}");
+        if (letterGrade !="F"){Console.WriteLine("you have passed");}
+        else {Console.WriteLine("you have failed");}
     }
 }
