40721bb39a45b6b5390638cf40a99d2c181497cb
jacktingey8 <jacktingey8@gmail.com>
2026-02-26 18:31:00 -0700

    Completed List activity

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,24 @@
 using System;
-
+using System.Globalization;
+using System.Reflection.PortableExecutable;
+using System.Linq;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep4 World!");
-    }
+    {   
+        List<int>numberList = new List<int>();
+        bool done = false;
+        do{
+        Console.Write("enter number:");
+        int numberForList=int.Parse(Console.ReadLine());
+        numberList.Add(numberForList);
+        if (numberForList == 0){done= true;}
+        }while (!done);
+        foreach (int i in numberList){Console.WriteLine(i);}
+        int sum = 0;
+        foreach (int i in numberList){sum=i+sum;}
+        int avg = sum/numberList.Count;
+        int max= numberList.Max();
+        Console.WriteLine($"Sum={sum} Average={avg} Maximum={max}");
+    }   
 }
