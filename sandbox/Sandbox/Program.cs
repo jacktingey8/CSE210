34cd6db919dcbba3673082e3287e762d9960fb83
jacktingey8 <jacktingey8@gmail.com>
2026-03-04 08:58:47 -0700

    sandbox changes

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -3,21 +3,49 @@ using System.Runtime.CompilerServices;
 
 class Program
 {
-    public static void Greetings()
-    {
-        Console.WriteLine("Hello");
-    }
+    // public static void Greetings()
+    // {
+    //     Console.WriteLine("Hello");
+    // }
 
-    public static int AddNumbers(int n1,int n2)
-    {
-       return(n1+n2);
-    }
+    // public static int AddNumbers(int n1,int n2)
+    // {
+    //    return(n1+n2);
+    // }
+
+    // public static int passByRef(ref int x){x ++; return x;}
+    // public static void passByOut(out int y)
+    // {
+    //     y=13;
+    //     }
+    // public static void passByValue(int a)
+    // {
+    //     a++;
+    //     Console.WriteLine($"vlue in function: {a}");
+    // }
+
     static void Main(string[] args)
     {
 
-        Greetings();
-        int total=AddNumbers(3,4);
-        Console.WriteLine(total);
+        Console.WriteLine("Circle");
+        Circle circle1 = new Circle();
+        circle1.SetRadius(10);
+        Console.WriteLine(circle1.GetRadius());
+        Console.WriteLine(circle1.GetArea());
+        Console.WriteLine(circle1.GetCircumference());
+        Console.WriteLine(circle1.GetDiameter());
+        // int x= 20;
+        // Console.WriteLine($"in main:{x}");
+        // passByValue(x);
+        // Console.WriteLine($"in main2:{x}");
+        // passByRef(ref x);
+        // Console.WriteLine($"in main3:{x}");
+        // passByOut(out x);
+        // Console.WriteLine($"in main4:{x}");
+
+        // Greetings();
+        // int total=AddNumbers(3,4);
+        // Console.WriteLine(total);
         // Console.Write("age:");
         // int age;
         // age = int.Parse(Console.ReadLine());
