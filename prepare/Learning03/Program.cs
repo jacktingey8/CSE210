c302554e2df198f53ec26bf7b45c30a8c5a3184f
jacktingey8 <jacktingey8@gmail.com>
2026-03-08 21:10:38 -0600

    Finished Fraction Practice

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction = new Fraction();
+        Random random = new Random();
+
+        for (int i=0; i < 20; i++)
+        {
+            fraction.SetTop(random.Next(50)); 
+            fraction.SetBottom(random.Next(50)); 
+            Console.WriteLine($"Fraction {i}: String: {fraction.GetFractionString()} Decimal: {fraction.GetFractionDecimal()} ");
+        
+        }
+
+        
     }
 }
