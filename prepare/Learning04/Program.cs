a03275df1ae4e3ab5c6253477fbeb15c14d7256a
jacktingey8 <jacktingey8@gmail.com>
2026-03-15 13:44:53 -0600

    inheritance assignment practice!!!!

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,33 @@
 using System;
 
 class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning04 World!");
+{static void Main(string[] args)
+{        
+//  Assignment assignment = new Assignment("Jack", "subtraction");
+
+// Console.WriteLine(assignment.GetSummary());
+
+MathAssignment maths = new MathAssignment("Jack","nubmers","6489539853","359244242348");
+
+Console.WriteLine(maths.GetSummary() +' '+ maths.GetHomeworkList());
+
+
+WritingAssignment words = new WritingAssignment("jack","book","i killed mockingbird!");
+
+Console.WriteLine(words.GetSummary()+ ' ' + words.GetWrtingInformation());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
